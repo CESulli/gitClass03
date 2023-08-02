47d9bc8bc9fdae3285033f3fdcdfd99409350767
CESulli <clare24@gmail.com>
2023-08-02 19:22:12 -0400

    The Code That Tells Me Product Availibility

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean product=true;
+
+        if (product) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
